385536f51b5f8833929455604d8eacb4f2635dd8
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-09-04 04:42:38 +0000

    Successfully added the hovering effect and a trail of lightcoral on the Pad

--- Project/etch-a-sketch/script.js
+++ Project/etch-a-sketch/script.js
@@ -1,8 +1,22 @@
-document.addEventListener('DOMContentLoaded', function manyDivs(){
-    const container = document.querySelector('.container');
-    for(let i =0; i < 256; i++){
-        const manyDiv = document.createElement('div');
-        manyDiv.classList.add('grid-square'); // for styling
-        container.appendChild(manyDiv);
-    }
+document.addEventListener("DOMContentLoaded", function manyDivs() {
+  const container = document.querySelector(".container");
+  for (let i = 0; i < 256; i++) {
+    const manyDiv = document.createElement("div");
+    manyDiv.classList.add("grid-square"); // for styling
+    container.appendChild(manyDiv);
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const grids = document.querySelectorAll(".grid-square");
+
+  grids.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "lightblue";
+    });
+
+    square.addEventListener("mouseout", () => {
+      square.style.backgroundColor = "lightcoral";
+    });
+  });
 });
