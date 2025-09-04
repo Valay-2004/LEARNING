6bf9d6173a337ccbc2d3a719218415b27a9f3b9c
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-09-04 03:49:49 +0000

    etch-a-sketch

---
+++ Project/etch-a-sketch/script.js
@@ -0,0 +1,9 @@
+function manyDivs(){
+    const container = document.querySelector('.container');
+    for(let i =0; i < 256; i++){
+        const manyDiv = document.createElement('div');
+        manyDiv.classList.add('grid-square'); // for styling
+        container.appendChild(manyDiv);
+    }
+}
+
