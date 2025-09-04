01ada4033c4fb8e155381d42bbb23e9f40841dfa
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-09-04 04:19:12 +0000

    Successfully added 16x16

--- Project/etch-a-sketch/script.js
+++ Project/etch-a-sketch/script.js
@@ -1,9 +1,8 @@
-function manyDivs(){
+document.addEventListener('DOMContentLoaded', function manyDivs(){
     const container = document.querySelector('.container');
     for(let i =0; i < 256; i++){
         const manyDiv = document.createElement('div');
         manyDiv.classList.add('grid-square'); // for styling
         container.appendChild(manyDiv);
     }
-}
-
+});
